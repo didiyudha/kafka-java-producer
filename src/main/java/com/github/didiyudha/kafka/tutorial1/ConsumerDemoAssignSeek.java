c6439737dcb645967f9f5626b1d5e71244b9cf6f
Didi Yudha Perwira <didiyudha@gmail.com>
2019-04-08 08:48:33 +0700

    assign and seek

---
+++ src/main/java/com/github/didiyudha/kafka/tutorial1/ConsumerDemoAssignSeek.java
@@ -0,0 +1,68 @@
+package com.github.didiyudha.kafka.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemoAssignSeek {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ConsumerDemoAssignSeek.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "my-seven-application";
+        String offsetConfig = "earliest";
+        String topic = "first_topic";
+
+        // Create consumer config.
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offsetConfig);
+
+        // Create consumer.
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(properties);
+
+        // assign and seek are mostly used to replay data or fetch a specific message.
+
+        // assign
+        TopicPartition partitionToReadFrom = new TopicPartition(topic, 0);
+        long offsetToReadFrom = 15L;
+
+        kafkaConsumer.assign(Arrays.asList(partitionToReadFrom));
+        kafkaConsumer.seek(partitionToReadFrom, offsetToReadFrom);
+
+        // Subscribe consumer to our topic(s).
+        // kafkaConsumer.subscribe(Arrays.asList(topic));
+
+        int numberOfMessagesToRead = 5;
+        boolean keepOnReading = true;
+        int numberOfMessagesReadSoFar = 0;
+
+        // Pool for new data.
+        while (keepOnReading) {
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                numberOfMessagesReadSoFar++;
+                logger.info("Key: "+ record.key()+ ", Value: "+record.value()+", Partition: "+ record.partition()+ ", " +
+                        "Offset: "+record.offset());
+                if (numberOfMessagesReadSoFar == numberOfMessagesToRead) {
+                    keepOnReading = false;
+                    break;
+                }
+            }
+        }
+
+        logger.info("Exiting the application");
+    }
+}
