a1f5a7479d18c5cc0294d488d4e8c96625436d21
Didi Yudha Perwira <didiyudha@gmail.com>
2019-04-12 08:32:52 +0700

    create safe producer

--- src/main/java/com/github/didiyudha/kafka/tutorial2/TwitterProducer.java
+++ src/main/java/com/github/didiyudha/kafka/tutorial2/TwitterProducer.java
@@ -124,6 +124,12 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.ACKS_CONFIG, acks);
 
+        // create safe producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
         // Create the producer.
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
         return producer;
